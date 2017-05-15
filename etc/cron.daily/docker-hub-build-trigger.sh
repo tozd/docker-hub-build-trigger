4cbc57d2127fe69a103a84c55a0c7537b0779657
Mitar <mitar.git@tnode.com>
2017-05-15 03:14:05 -0700

    Run as nobody.

--- etc/cron.daily/docker-hub-build-trigger.sh
+++ etc/cron.daily/docker-hub-build-trigger.sh
@@ -3,5 +3,5 @@
 . /dev/shm/cron-environment
 
 if [[ -n "$DOCKER_IMAGE" && -n "$TRIGGER_TOKEN" ]]; then
-  /usr/bin/curl --output /dev/null --silent -H "Content-Type: application/json" --data '{"build": true}' -X POST "https://registry.hub.docker.com/u/${DOCKER_IMAGE}/trigger/${TRIGGER_TOKEN}/"
+  chpst -u nobody:nogroup /usr/bin/curl --output /dev/null --silent -H "Content-Type: application/json" --data '{"build": true}' -X POST "https://registry.hub.docker.com/u/${DOCKER_IMAGE}/trigger/${TRIGGER_TOKEN}/"
 fi
